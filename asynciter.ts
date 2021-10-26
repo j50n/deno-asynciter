2ff4b80cabb97f72859f8e00bed2e5e0a45c44f6
j50n <j50n@lilypepper.com>
2021-10-25 20:12:39 -0600

    1 refactor project

--- asynciter.ts
+++ asynciter.ts
@@ -43,15 +43,13 @@ export class AsyncIterableExtras<T> implements AsyncIterable<T> {
   /**
    * Map the sequence from one type to another.
    * @param mapFn The mapping function.
-   * @returns An iterator of mapped values.
+   * @returns An iterable of mapped values.
    */
   public map<U>(mapFn: (item: T) => U | Promise<U>): AsyncIterableExtras<U> {
-    const iterator = this.iterator;
+    const iterable = this.iterator;
     return new AsyncIterableExtras({
       async *[Symbol.asyncIterator]() {
-        for await (const item of iterator) {
-          yield await mapFn(item);
-        }
+        yield* map(iterable, mapFn);
       },
     });
   }
@@ -125,5 +123,17 @@ export class AsyncIterableExtras<T> implements AsyncIterable<T> {
       result.push(item);
     }
     return result;
+  }
+}
+
+ /**
+   * Map the sequence from one type to another.
+   * @param iterable An iterable collection.
+   * @param mapFn The mapping function.
+   * @returns An iterator of mapped values.
+   */
+export async function* map<T,U>(iterable: AsyncIterable<T>, mapFn: (item: T) => U | Promise<U>): AsyncIterableIterator<U> {
+  for await (const item of iterable) {
+    yield await mapFn(item);
   }
 }
--- mod.ts
+++ mod.ts
@@ -1,3 +1,2 @@
 export * from "./asynciter.ts";
-export * from "./sh.ts";
-export * from "./util.ts";
+
